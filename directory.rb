6eaf2de6ba929845bfea4f8c24f9645a4bea65c2
Joseph Lander <josephclander@gmail.com>
2022-09-27 12:55:00 +0100

    Issue 8: Make filter for options displayed

--- directory.rb
+++ directory.rb
@@ -18,7 +18,7 @@ def print(students)
   index = 0
   while index < students.size
     student = students[index]
-    # student = { name: name, age: age, pob: pob, cohort: :november }
+    # student = { name: name, age: age, pob: pob, cohort: cohort }
     details = student.map { |key, value| "#{key}: ".green + "#{value}" }
     puts "#{index + 1}. " + details.join(', ')
     index += 1
@@ -76,9 +76,29 @@ def search_length(full_list)
   new_list
 end
 
+# return only given info selection
+def search_info(full_list)
+  puts 'Choose only one category?'.green
+  puts 'age | pob | cohort'.red
+  puts 'For all, hit return'.green
+  input = gets.chomp
+  new_list = full_list
+  if input != 0 && %w[age pob cohort].include?(input)
+    new_list = full_list.map do |student|
+      filtered_student = {}
+      # student = { name: name, age: age, pob: pob, cohort: cohort }
+      filtered_student[:name] = student[:name]
+      filtered_student[input.to_sym] = student[input.to_sym]
+      filtered_student
+    end
+  end
+  new_list
+end
+
 students = input_students
 letter_list = search_letter(students)
 length_list = search_length(letter_list)
+info_list = search_info(length_list)
 print_header
-print(length_list)
+print(info_list)
 print_footer(students)
